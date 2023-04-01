8149feb4e301f696ffcc872e770131f3d6a36dd2
bintang <56790951+bintangyp@users.noreply.github.com>
2023-04-02 06:39:44 +0700

    dev 6.2

--- config/Database.js
+++ config/Database.js
@@ -3,6 +3,7 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize("sql12610384", "sql12610384", "j5xq82Emie", {
   host: "sql12.freesqldatabase.com",
   port: "3306",
+  dialect: "mysql",
 });
 
 export default db;
